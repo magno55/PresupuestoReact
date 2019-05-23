07c5a382f50ff7869a0fc6881fff8987ac022de6
Magno I. Taipe Charca <mtaipe05@gmail.com>
2019-05-23 15:12:40 -0500

    Proyecto finalizado v1

---
+++ precache-manifest.6d8ab2d5ec9965c46e3bfc3fe842cc08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ae72eb4c0e5763a982302e0afb45af5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdec46705c40b9df1eb2",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "92625e8f421b478da1a1",
+    "url": "/static/js/2.5ca6e9a6.chunk.js"
+  },
+  {
+    "revision": "fdec46705c40b9df1eb2",
+    "url": "/static/js/main.4e9771a3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
